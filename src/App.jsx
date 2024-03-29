c8480756a4503b2320f97664f73466be01b16f0c
Jason Fine <jasonfine94@gmail.com>
2024-03-28 21:51:28 -0400

    react tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="grid grid-cols-2 gap-12">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2 className="text-2xl font-bold">Barbie</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2 className="text-2xl font-bold">Oppenheimer</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
