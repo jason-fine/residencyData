794bb39c4d09fc53d53c889bf8ac671f97fcd249
Jason Fine <jasonfine94@gmail.com>
2024-03-31 20:52:48 -0400

    added line graph charting match rate changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,23 @@ import './App.css'
 
 import mdData from './data/md/combined_data.json'
 import doData from './data/do/combined_data.json'
+import numberData from './data/combined_numbers/combined_data.json'
+import data2020 from './data/numbers_jsons/2020.json'
+import data2021 from './data/numbers_jsons/2021.json'
+import data2022 from './data/numbers_jsons/2022.json'
+import data2023 from './data/numbers_jsons/2023.json'
 
 import { Card, Metric, Text } from "@tremor/react";
+import { LineChart } from '@tremor/react';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   const [selectedSpecialty, setSelectedSpecialty] = useState('Total PGY-1');
   const [selectedYear, setSelectedYear] = useState(2024);
+  const [selectedSpecialtyChart, setSelectedSpecialtyChart] = useState('Total PGY-1');
+  const [selectedSpecialtyChartQuestion, setSelectedSpecialtyChartQuestion] = useState('Matched MDs');
 
   const handleSpecialtyChange = (event) => {
     setSelectedSpecialty(event.target.value);
@@ -21,6 +30,22 @@ function App() {
   const handleYearChange = (event) => {
     setSelectedYear(parseInt(event.target.value));
   };
+
+  const handleSpecialtyChartChange = (event) => {
+    setSelectedSpecialtyChart(event.target.value);
+  };
+
+  const handleSpecialtyChartQuestionChange = (event) => {
+    setSelectedSpecialtyChartQuestion(event.target.value);
+  };
+
+  const combinedNumberData = [];
+  combinedNumberData.push(data2020);
+  combinedNumberData.push(data2021);
+  combinedNumberData.push(data2022);
+  combinedNumberData.push(data2023);
+
+
 
 
   return (
@@ -117,7 +142,61 @@ function App() {
           </Card>
         </div>
       </div>
-      <h2 className="text-2xl font-bold mb-6">Tracking Match Rates By Year</h2>
+      <h2 className="text-2xl font-bold mb-6">Tracking MD VS DO Match Rates By Year</h2>
+      <div className="grid grid-cols-2 gap-12">
+        <h4 className="text-2xl font-bold mb-6">Select Medical Specialty</h4>
+            <select value={selectedSpecialtyChart} onChange={handleSpecialtyChartChange} className="mb-6">
+              <option value="Anesthesiology">Anesthesiology</option>
+              <option value="Child Neurology">Child Neurology</option>
+              <option value="Dermatology">Dermatology</option>
+              <option value="Emergency Medicine">Emergency Medicine</option>
+              <option value="Family Medicine">Family Medicine</option>
+              <option value="Internal Medicine">Internal Medicine</option>
+              <option value="Internal Medicine (Prelim)">Internal Medicine (Prelim)</option>
+              <option value="Internal Medicine/Emergency Medicine">Internal Medicine/Emergency Medicine</option>
+              <option value="Internal Medicine/Pediatrics">Internal Medicine/Pediatrics</option>
+              <option value="Internal Medicine/Psychiatry">Internal Medicine/Psychiatry</option>
+              <option value="Interventional Radiology">Interventional Radiology</option>
+              <option value="Neurological Surgery">Neurological Surgery</option>
+              <option value="Neurology">Neurology</option>
+              <option value="Obstetrics and Gynecology">Obstetrics and Gynecology</option>
+              <option value="Orthopaedic Surgery">Orthopaedic Surgery</option>
+              <option value="Osteo Neuromusculoskeletal Medicine">Osteo Neuromusculoskeletal Medicine</option>
+              <option value="Otolaryngology">Otolaryngology</option>
+              <option value="Pathology-Anatomic and Clinical">Pathology-Anatomic and Clinical</option>
+              <option value="Pediatrics">Pediatrics</option>
+              <option value="Pediatrics (Prelim)">Pediatrics (Prelim)</option>
+              <option value="Pediatrics/Medical Genetics">Pediatrics/Medical Genetics</option>
+              <option value="Pediatrics/Psychiatry/Child and Adolescent Psychiatry">Pediatrics/Psychiatry/Child and Adolescent Psychiatry</option>
+              <option value="Physical Medicine and Rehabilitation">Physical Medicine and Rehabilitation</option>
+              <option value="Plastic Surgery">Plastic Surgery</option>
+              <option value="Psychiatry">Psychiatry</option>
+              <option value="Radiation Oncology">Radiation Oncology</option>
+              <option value="Radiology-Diagnostic">Radiology-Diagnostic</option>
+              <option value="Surgery-General">Surgery-General</option>
+              <option value="Surgery-General (Prelim)">Surgery-General (Prelim)</option>
+              <option value="Thoracic Surgery">Thoracic Surgery</option>
+              <option value="Vascular Surgery">Vascular Surgery</option>
+              <option value="Total PGY-1">Total PGY-1</option>
+            </select>
+ 
+        <LineChart
+          className="mt-4 h-72"
+          data={combinedNumberData}
+          index="Year"
+          yAxisWidth={65}
+          categories={[`${selectedSpecialtyChart}.Matched MDs`,`${selectedSpecialtyChart}.Unmatched MDs`,`${selectedSpecialtyChart}.Total MDs`]}
+          colors={['indigo','orange','cyan']}
+        />
+        <LineChart
+          className="mt-4 h-72"
+          data={combinedNumberData}
+          index="Year"
+          yAxisWidth={65}
+          categories={[`${selectedSpecialtyChart}.Matched DOs`,`${selectedSpecialtyChart}.Unmatched DOs`,`${selectedSpecialtyChart}.Total DOs`]}
+          colors={['indigo','orange','cyan']}
+        />
+      </div>
     </div>
     
     
