b69d37eae969ca6813a74b4dd78c8bc71ae63144
Jason Fine <jasonfine94@gmail.com>
2024-04-03 03:04:26 -0400

    still working on last graph. need to find out why data is not showing up in graph. also planning to add a graph below that shows percentage change over time

--- src/App.jsx
+++ src/App.jsx
@@ -13,49 +13,48 @@ import data2023 from './data/numbers_jsons/2023.json'
 
 
 //new data
-// import longtermDo2004 from './data/';
-// C:\Users\jason\Documents\6440\practicumCode\residencyData\data\lastdatatoadd\do\2004.json
-// import longtermDo2005 from './data/lastdatatoadd/do/2005.json';
-// import longtermDo2006 from './data/lastdatatoadd/do/2006.json';
-// import longtermDo2007 from './data/lastdatatoadd/do/2007.json';
-// import longtermDo2008 from './data/lastdatatoadd/do/2008.json';
-// import longtermDo2009 from './data/lastdatatoadd/do/2009.json';
-// import longtermDo2010 from './data/lastdatatoadd/do/2010.json';
-// import longtermDo2011 from './data/lastdatatoadd/do/2011.json';
-// import longtermDo2012 from './data/lastdatatoadd/do/2012.json';
-// import longtermDo2013 from './data/lastdatatoadd/do/2013.json';
-// import longtermDo2014 from './data/lastdatatoadd/do/2014.json';
-// import longtermDo2015 from './data/lastdatatoadd/do/2015.json';
-// import longtermDo2016 from './data/lastdatatoadd/do/2016.json';
-// import longtermDo2017 from './data/lastdatatoadd/do/2017.json';
-// import longtermDo2018 from './data/lastdatatoadd/do/2018.json';
-// import longtermDo2019 from './data/lastdatatoadd/do/2019.json';
-// import longtermDo2020 from './data/lastdatatoadd/do/2020.json';
-// import longtermDo2021 from './data/lastdatatoadd/do/2021.json';
-// import longtermDo2022 from './data/lastdatatoadd/do/2022.json';
-// import longtermDo2023 from './data/lastdatatoadd/do/2023.json';
+import longtermDo2004 from './data/lastdatatoadd/do/2004.json';
+import longtermDo2005 from './data/lastdatatoadd/do/2005.json';
+import longtermDo2006 from './data/lastdatatoadd/do/2006.json';
+import longtermDo2007 from './data/lastdatatoadd/do/2007.json';
+import longtermDo2008 from './data/lastdatatoadd/do/2008.json';
+import longtermDo2009 from './data/lastdatatoadd/do/2009.json';
+import longtermDo2010 from './data/lastdatatoadd/do/2010.json';
+import longtermDo2011 from './data/lastdatatoadd/do/2011.json';
+import longtermDo2012 from './data/lastdatatoadd/do/2012.json';
+import longtermDo2013 from './data/lastdatatoadd/do/2013.json';
+import longtermDo2014 from './data/lastdatatoadd/do/2014.json';
+import longtermDo2015 from './data/lastdatatoadd/do/2015.json';
+import longtermDo2016 from './data/lastdatatoadd/do/2016.json';
+import longtermDo2017 from './data/lastdatatoadd/do/2017.json';
+import longtermDo2018 from './data/lastdatatoadd/do/2018.json';
+import longtermDo2019 from './data/lastdatatoadd/do/2019.json';
+import longtermDo2020 from './data/lastdatatoadd/do/2020.json';
+import longtermDo2021 from './data/lastdatatoadd/do/2021.json';
+import longtermDo2022 from './data/lastdatatoadd/do/2022.json';
+import longtermDo2023 from './data/lastdatatoadd/do/2023.json';
 
 
-// import longtermMd2004 from './data/lastdatatoadd/md/2004.json';
-// import longtermMd2005 from './data/lastdatatoadd/md/2005.json';
-// import longtermMd2006 from './data/lastdatatoadd/md/2006.json';
-// import longtermMd2007 from './data/lastdatatoadd/md/2007.json';
-// import longtermMd2008 from './data/lastdatatoadd/md/2008.json';
-// import longtermMd2009 from './data/lastdatatoadd/md/2009.json';
-// import longtermMd2010 from './data/lastdatatoadd/md/2010.json';
-// import longtermMd2011 from './data/lastdatatoadd/md/2011.json';
-// import longtermMd2012 from './data/lastdatatoadd/md/2012.json';
-// import longtermMd2013 from './data/lastdatatoadd/md/2013.json';
-// import longtermMd2014 from './data/lastdatatoadd/md/2014.json';
-// import longtermMd2015 from './data/lastdatatoadd/md/2015.json';
-// import longtermMd2016 from './data/lastdatatoadd/md/2016.json';
-// import longtermMd2017 from './data/lastdatatoadd/md/2017.json';
-// import longtermMd2018 from './data/lastdatatoadd/md/2018.json';
-// import longtermMd2019 from './data/lastdatatoadd/md/2019.json';
-// import longtermMd2020 from './data/lastdatatoadd/md/2020.json';
-// import longtermMd2021 from './data/lastdatatoadd/md/2021.json';
-// import longtermMd2022 from './data/lastdatatoadd/md/2022.json';
-// import longtermMd2023 from './data/lastdatatoadd/md/2023.json';
+import longtermMd2004 from './data/lastdatatoadd/md/2004.json';
+import longtermMd2005 from './data/lastdatatoadd/md/2005.json';
+import longtermMd2006 from './data/lastdatatoadd/md/2006.json';
+import longtermMd2007 from './data/lastdatatoadd/md/2007.json';
+import longtermMd2008 from './data/lastdatatoadd/md/2008.json';
+import longtermMd2009 from './data/lastdatatoadd/md/2009.json';
+import longtermMd2010 from './data/lastdatatoadd/md/2010.json';
+import longtermMd2011 from './data/lastdatatoadd/md/2011.json';
+import longtermMd2012 from './data/lastdatatoadd/md/2012.json';
+import longtermMd2013 from './data/lastdatatoadd/md/2013.json';
+import longtermMd2014 from './data/lastdatatoadd/md/2014.json';
+import longtermMd2015 from './data/lastdatatoadd/md/2015.json';
+import longtermMd2016 from './data/lastdatatoadd/md/2016.json';
+import longtermMd2017 from './data/lastdatatoadd/md/2017.json';
+import longtermMd2018 from './data/lastdatatoadd/md/2018.json';
+import longtermMd2019 from './data/lastdatatoadd/md/2019.json';
+import longtermMd2020 from './data/lastdatatoadd/md/2020.json';
+import longtermMd2021 from './data/lastdatatoadd/md/2021.json';
+import longtermMd2022 from './data/lastdatatoadd/md/2022.json';
+import longtermMd2023 from './data/lastdatatoadd/md/2023.json';
 
 //end of new data
 
@@ -70,6 +69,7 @@ function App() {
   const [selectedYear, setSelectedYear] = useState(2024);
   const [selectedSpecialtyChart, setSelectedSpecialtyChart] = useState('Total PGY-1');
   const [selectedSpecialtyChartQuestion, setSelectedSpecialtyChartQuestion] = useState('Matched MDs');
+  const [selectedLongtermChart, setSelectedLongtermChart] = useState('Anesthesiology');
 
   const handleSpecialtyChange = (event) => {
     setSelectedSpecialty(event.target.value);
@@ -87,11 +87,62 @@ function App() {
     setSelectedSpecialtyChartQuestion(event.target.value);
   };
 
+  const handleLongtermChange = (event) => {
+    setSelectedLongtermChart(event.target.value);
+  };
+
   const combinedNumberData = [];
   combinedNumberData.push(data2020);
   combinedNumberData.push(data2021);
   combinedNumberData.push(data2022);
   combinedNumberData.push(data2023);
+
+  const combinedLastDOData = [];
+  combinedLastDOData.push(longtermDo2004);
+  combinedLastDOData.push(longtermDo2005);
+  combinedLastDOData.push(longtermDo2006);
+  combinedLastDOData.push(longtermDo2007);
+  combinedLastDOData.push(longtermDo2008);
+  combinedLastDOData.push(longtermDo2009);
+  combinedLastDOData.push(longtermDo2010);
+  combinedLastDOData.push(longtermDo2011);
+  combinedLastDOData.push(longtermDo2012);
+  combinedLastDOData.push(longtermDo2013);
+  combinedLastDOData.push(longtermDo2014);
+  combinedLastDOData.push(longtermDo2015);
+  combinedLastDOData.push(longtermDo2016);
+  combinedLastDOData.push(longtermDo2017);
+  combinedLastDOData.push(longtermDo2018);
+  combinedLastDOData.push(longtermDo2019);
+  combinedLastDOData.push(longtermDo2020);
+  combinedLastDOData.push(longtermDo2021);
+  combinedLastDOData.push(longtermDo2022);
+  combinedLastDOData.push(longtermDo2023);
+
+  const combinedLastMdData = [];
+  combinedLastMdData.push(longtermMd2004);
+  combinedLastMdData.push(longtermMd2005);
+  combinedLastMdData.push(longtermMd2006);
+  combinedLastMdData.push(longtermMd2007);
+  combinedLastMdData.push(longtermMd2008);
+  combinedLastMdData.push(longtermMd2009);
+  combinedLastMdData.push(longtermMd2010);
+  combinedLastMdData.push(longtermMd2011);
+  combinedLastMdData.push(longtermMd2012);
+  combinedLastMdData.push(longtermMd2013);
+  combinedLastMdData.push(longtermMd2014);
+  combinedLastMdData.push(longtermMd2015);
+  combinedLastMdData.push(longtermMd2016);
+  combinedLastMdData.push(longtermMd2017);
+  combinedLastMdData.push(longtermMd2018);
+  combinedLastMdData.push(longtermMd2019);
+  combinedLastMdData.push(longtermMd2020);
+  combinedLastMdData.push(longtermMd2021);
+  combinedLastMdData.push(longtermMd2022);
+  combinedLastMdData.push(longtermMd2023);
+
+  {console.log(combinedLastMdData)}
+  
 
 
 
@@ -190,7 +241,7 @@ function App() {
           </Card>
         </div>
       </div>
-      <h2 className="text-2xl font-bold mb-6">Tracking MD VS DO Match Rates By Year</h2>
+      <h2 className="text-2xl font-bold mb-6">MD VS DO Match and Unmatched Rates By Year</h2>
       <div className="grid grid-cols-2 gap-12">
         <h4 className="text-2xl font-bold mb-6">Select Medical Specialty</h4>
             <select value={selectedSpecialtyChart} onChange={handleSpecialtyChartChange} className="mb-6">
@@ -244,6 +295,62 @@ function App() {
           yAxisWidth={65}
           categories={[`${selectedSpecialtyChart}.Matched DOs`,`${selectedSpecialtyChart}.Unmatched DOs`,`${selectedSpecialtyChart}.Total DOs`]}
           colors={['indigo','orange','cyan']}
+        />
+      </div>
+
+      <h2 className="text-2xl font-bold mb-6">Longterm MD VS DO Total Matches</h2>
+      <div className="grid grid-cols-2 gap-12">
+        <h4 className="text-2xl font-bold mb-6">Select Medical Specialty</h4>
+            <select value={selectedLongtermChart} onChange={handleLongtermChange} className="mb-6">
+              <option value="Anesthesiology">Anesthesiology</option>
+              <option value="Child Neurology">Child Neurology</option>
+              <option value="Dermatology">Dermatology</option>
+              <option value="Emergency Medicine">Emergency Medicine</option>
+              <option value="Family Medicine">Family Medicine</option>
+              <option value="Internal Medicine">Internal Medicine</option>
+              <option value="Internal Medicine (Prelim)">Internal Medicine (Prelim)</option>
+              <option value="Internal Medicine/Emergency Medicine">Internal Medicine/Emergency Medicine</option>
+              <option value="Internal Medicine/Pediatrics">Internal Medicine/Pediatrics</option>
+              <option value="Internal Medicine/Psychiatry">Internal Medicine/Psychiatry</option>
+              <option value="Interventional Radiology">Interventional Radiology</option>
+              <option value="Neurological Surgery">Neurological Surgery</option>
+              <option value="Neurology">Neurology</option>
+              <option value="Obstetrics and Gynecology">Obstetrics and Gynecology</option>
+              <option value="Orthopaedic Surgery">Orthopaedic Surgery</option>
+              <option value="Osteo Neuromusculoskeletal Medicine">Osteo Neuromusculoskeletal Medicine</option>
+              <option value="Otolaryngology">Otolaryngology</option>
+              <option value="Pathology-Anatomic and Clinical">Pathology-Anatomic and Clinical</option>
+              <option value="Pediatrics">Pediatrics</option>
+              <option value="Pediatrics (Prelim)">Pediatrics (Prelim)</option>
+              <option value="Pediatrics/Medical Genetics">Pediatrics/Medical Genetics</option>
+              <option value="Pediatrics/Psychiatry/Child and Adolescent Psychiatry">Pediatrics/Psychiatry/Child and Adolescent Psychiatry</option>
+              <option value="Physical Medicine and Rehabilitation">Physical Medicine and Rehabilitation</option>
+              <option value="Plastic Surgery">Plastic Surgery</option>
+              <option value="Psychiatry">Psychiatry</option>
+              <option value="Radiation Oncology">Radiation Oncology</option>
+              <option value="Radiology-Diagnostic">Radiology-Diagnostic</option>
+              <option value="Surgery-General">Surgery-General</option>
+              <option value="Surgery-General (Prelim)">Surgery-General (Prelim)</option>
+              <option value="Thoracic Surgery">Thoracic Surgery</option>
+              <option value="Vascular Surgery">Vascular Surgery</option>
+            </select>
+ 
+        <LineChart
+          className="mt-4 h-72"
+          data={combinedLastMdData}
+          index="year"
+          yAxisWidth={65}
+          categories={[`results.${selectedLongtermChart}.Percentage_of_Matches`]}
+          colors={['indigo']}
+          
+        />
+        <LineChart
+          className="mt-4 h-72"
+          data={combinedLastDOData}
+          index="year"
+          yAxisWidth={65}
+          categories={[`${selectedLongtermChart}.Number_Matched`]}
+          colors={['indigo']}
         />
       </div>
     </div>
