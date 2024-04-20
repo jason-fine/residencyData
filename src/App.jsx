21bcf04f24325f30be6a2db774cbff0037fdd954
Jason Fine <jasonfine94@gmail.com>
2024-04-20 13:01:15 -0400

    Fixed historical graph

--- src/App.jsx
+++ src/App.jsx
@@ -141,7 +141,6 @@ function App() {
   combinedLastMdData.push(longtermMd2022);
   combinedLastMdData.push(longtermMd2023);
 
-  {console.log(combinedLastMdData)}
   
 
 
@@ -149,6 +148,12 @@ function App() {
 
   return (
     <div>
+      <div>
+        <h1>Welcome to ResidencyData!</h1>
+        <br></br>
+        <h2>Take a look at residency match rates since 2020 when NRMP undertook MD and DO matches and historically.</h2>
+        <br></br>
+      </div>
       <div className="grid grid-cols-3 gap-12">
         <div>
         <h2 className="text-2xl font-bold mb-6">Select Medical Specialty and Year</h2>
@@ -221,22 +226,22 @@ function App() {
         <div>
           <h2 className="text-2xl font-bold mb-6">MD</h2>
           <Card className="mx-auto max-w-xs mb-6" decoration="top" decorationColor="indigo">
-            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">2024 Applicants in {selectedSpecialty}</p>
+            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">Applicants in {selectedSpecialty}</p>
             <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">{mdData[selectedYear][selectedSpecialty].No_of_Applicants}</p>
           </Card>
           <Card className="mx-auto max-w-xs" decoration="top" decorationColor="indigo">
-            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">2024 Percentage of Positions Filled by Senior MDs in {selectedSpecialty}</p>
+            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">Percentage of Positions Filled by Senior MDs in {selectedSpecialty}</p>
             <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">{mdData[selectedYear][selectedSpecialty].Percentage_Filled}%</p>
           </Card>
         </div>
         <div>
           <h2 className="text-2xl font-bold mb-6">DO</h2>
           <Card className="mx-auto max-w-xs mb-6" decoration="top" decorationColor="indigo">
-            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">2024 Applicants in {selectedSpecialty}</p>
+            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">Applicants in {selectedSpecialty}</p>
             <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">{doData[selectedYear][selectedSpecialty].No_of_Applicants}</p>
           </Card>
           <Card className="mx-auto max-w-xs" decoration="top" decorationColor="indigo">
-            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">2024 Percentage of Positions Filled by Senior DOs in {selectedSpecialty}</p>
+            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">Percentage of Positions Filled by Senior DOs in {selectedSpecialty}</p>
             <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">{doData[selectedYear][selectedSpecialty].Percentage_Filled}%</p>
           </Card>
         </div>
@@ -302,37 +307,74 @@ function App() {
       <div className="grid grid-cols-2 gap-12">
         <h4 className="text-2xl font-bold mb-6">Select Medical Specialty</h4>
             <select value={selectedLongtermChart} onChange={handleLongtermChange} className="mb-6">
-              <option value="Anesthesiology">Anesthesiology</option>
-              <option value="Child Neurology">Child Neurology</option>
-              <option value="Dermatology">Dermatology</option>
-              <option value="Emergency Medicine">Emergency Medicine</option>
-              <option value="Family Medicine">Family Medicine</option>
-              <option value="Internal Medicine">Internal Medicine</option>
-              <option value="Internal Medicine (Prelim)">Internal Medicine (Prelim)</option>
-              <option value="Internal Medicine/Emergency Medicine">Internal Medicine/Emergency Medicine</option>
-              <option value="Internal Medicine/Pediatrics">Internal Medicine/Pediatrics</option>
-              <option value="Internal Medicine/Psychiatry">Internal Medicine/Psychiatry</option>
-              <option value="Interventional Radiology">Interventional Radiology</option>
-              <option value="Neurological Surgery">Neurological Surgery</option>
-              <option value="Neurology">Neurology</option>
-              <option value="Obstetrics and Gynecology">Obstetrics and Gynecology</option>
-              <option value="Orthopaedic Surgery">Orthopaedic Surgery</option>
-              <option value="Osteo Neuromusculoskeletal Medicine">Osteo Neuromusculoskeletal Medicine</option>
-              <option value="Otolaryngology">Otolaryngology</option>
-              <option value="Pathology-Anatomic and Clinical">Pathology-Anatomic and Clinical</option>
-              <option value="Pediatrics">Pediatrics</option>
-              <option value="Pediatrics (Prelim)">Pediatrics (Prelim)</option>
-              <option value="Pediatrics/Medical Genetics">Pediatrics/Medical Genetics</option>
-              <option value="Pediatrics/Psychiatry/Child and Adolescent Psychiatry">Pediatrics/Psychiatry/Child and Adolescent Psychiatry</option>
-              <option value="Physical Medicine and Rehabilitation">Physical Medicine and Rehabilitation</option>
-              <option value="Plastic Surgery">Plastic Surgery</option>
-              <option value="Psychiatry">Psychiatry</option>
-              <option value="Radiation Oncology">Radiation Oncology</option>
-              <option value="Radiology-Diagnostic">Radiology-Diagnostic</option>
-              <option value="Surgery-General">Surgery-General</option>
-              <option value="Surgery-General (Prelim)">Surgery-General (Prelim)</option>
-              <option value="Thoracic Surgery">Thoracic Surgery</option>
-              <option value="Vascular Surgery">Vascular Surgery</option>
+              <option value="Anesthesiology:">Anesthesiology</option>
+              <option value="Child Neurology:">Child Neurology</option>
+              <option value="Dermatology:">Dermatology</option>
+              <option value="Emergency Medicine:">Emergency Medicine</option>
+              <option value="Emergency Med-Anesthesiology:">Emergency Med-Anesthesiology:</option>
+              <option value="Emergency Med/Family Med:">Emergency Med/Family Med</option>
+              <option value="Family Medicine:">Family Medicine</option>
+              <option value="Family Med-Preventive Med:">Family Med-Preventive Med</option>
+              <option value="Internal Medicine (Categorical):">Internal Medicine (Categorical)</option>
+              <option value="Medicine-Anesthesiology:">Medicine-Anesthesiology</option>
+              <option value="Medicine-Dermatology:">Medicine-Dermatology</option>
+              <option value="Medicine-Emerg Med:">Medicine-Emerg Med</option>
+              <option value="Medicine-Emergency Med:">Medicine-Emergency Med:</option>
+              <option value="Medicine-Family Medicine:">Medicine-Family Medicine</option>
+              <option value="Medicine-Medical Genetics:">Medicine-Medical Genetics</option>
+
+              <option value="Medicine-Neurology:">Medicine-Neurology</option>
+              <option value="Medicine-P M & R:">Medicine-P M & R</option>
+              <option value="Medicine-Pediatrics:">Medicine-Pediatrics</option>
+              <option value="Medicine-Preliminary (PGY-1 Only):">Medicine-Preliminary (PGY-1 Only)</option>
+              <option value="Medicine-Preventive Med:">Medicine-Preventive Med</option>
+              <option value="Medicine-Primary:">Medicine-Primary</option>
+
+              <option value="Medicine-Psychiatry:">Medicine-Psychiatry</option>
+              <option value="Interventional Radiology (Integrated):">Interventional Radiology (Integrated)</option>
+
+
+
+              <option value="Medical Genetics:">Medical Genetics</option>
+              <option value="Neurodevelopmental Disabilities:">Neurodevelopmental Disabilities</option>
+              <option value="Neurological Surgery:">Neurological Surgery</option>
+              <option value="Neurology:">Neurology</option>
+              <option value="Nuclear Medicine:">Nuclear Medicine</option>
+              <option value="Obstetrics-Gynecology:">Obstetrics-Gynecology</option>
+              <option value="OB/GYN-Preliminary (PGY-1 Only):">OB/GYN-Preliminary (PGY-1 Only)</option>
+              <option value="Ophthalmology:">Ophthalmology</option>
+              <option value="Orthopedic Surgery:">Orthopedic Surgery:</option>
+              <option value="Osteo Neuromusculoskeletal Med:">Osteo Neuromusculoskeletal Med:</option>
+
+              <option value="Otolaryngology:">Otolaryngology</option>
+              <option value="Pathology:">Pathology</option>
+
+              <option value="Pediatrics (Categorical):">Pediatrics (Categorical)</option>
+              <option value="Pediatrics-Anesthesiology:">Pediatrics-Anesthesiology:</option>
+              <option value="Pediatrics-Dermatology:">Pediatrics-Dermatology</option>
+              <option value="Pediatrics-Emerg Med:">Pediatrics-Emerg Med</option>
+              <option value="Pediatrics-Emergency Med:">Pediatrics-Emergency Med</option>
+              <option value="Pediatrics-Medical Genetics:">Pediatrics-Medical Genetics</option>
+              <option value="Pediatrics-P M & R:">Pediatrics-P M & R</option>
+              <option value="Pediatrics-Preliminary:">Pediatrics-Preliminary:</option>
+              <option value="Pediatrics-Primary:">Pediatrics-Primary</option>
+              <option value="Peds/Psych/Child Psych:">Peds/Psych/Child Psych</option>
+              <option value="Physical Medicine & Rehab:">Physical Medicine & Rehab</option>
+              <option value="Plastic Surgery:">Plastic Surgery</option>
+              <option value="Plastic Surgery (Integrated):">Plastic Surgery (Integrated)</option>
+              <option value="Preventive Medicine:">Preventive Medicine</option>
+              <option value="Psychiatry:">Psychiatry</option>
+              <option value="Psychiatry (Categorical):">Psychiatry (Categorical)</option>
+              <option value="Psychiatry-Family Medicine:">Psychiatry-Family Medicine</option>
+              <option value="Psychiatry-Neurology:">Psychiatry-Neurology</option>
+              <option value="Radiation Oncology:">Radiation Oncology</option>
+              <option value="Radiology-Diagnostic:">Radiology-Diagnostic</option>
+              <option value="Surgery (Categorical):">Surgery (Categorical)</option>
+              <option value="Surgery-Preliminary (PGY-1 Only):">Surgery-Preliminary (PGY-1 Only)</option>
+              <option value="Thoracic Surgery:">Thoracic Surgery</option>
+              <option value="Urology:">Urology</option>
+              <option value="Vascular Surgery:">Vascular Surgery</option>
+              
             </select>
  
         <LineChart
@@ -340,7 +382,7 @@ function App() {
           data={combinedLastMdData}
           index="year"
           yAxisWidth={65}
-          categories={[`results.${selectedLongtermChart}.Percentage_of_Matches`]}
+          categories={[`results.${selectedLongtermChart}.Number_Matched`]}
           colors={['indigo']}
           
         />
@@ -349,7 +391,7 @@ function App() {
           data={combinedLastDOData}
           index="year"
           yAxisWidth={65}
-          categories={[`${selectedLongtermChart}.Number_Matched`]}
+          categories={[`results.${selectedLongtermChart}.Number_Matched`]}
           colors={['indigo']}
         />
       </div>
